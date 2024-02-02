81724d3a02ba858ae8dba5099dee94af67a2ea88
João Rafael <joaoraf@me.com>
2024-02-02 19:54:37 -0300

    refactor: update type imports and definitions in bot settings.py

--- bot/settings.py
+++ bot/settings.py
@@ -1,15 +1,16 @@
 import dj_database_url
 
-from pydantic import PyObject
+from typing import Dict
+from pydantic import ImportString
 from pydantic_settings import BaseSettings
 
 
 class Conf(BaseSettings):
     DEBUG: bool = False
-    DATABASES: dict = {"default": dj_database_url.config()}
+    DATABASES: Dict = {"default": dj_database_url.config()}
     INSTALLED_APPS: tuple = ("bot",)
     SECRET_KEY: str = "secret"
-    LOG_LEVEL: PyObject = "logging.INFO"
+    LOG_LEVEL: ImportString = "logging.INFO"
     TELEGRAM_TOKEN: str = ...
     USE_TZ: bool = True
 
