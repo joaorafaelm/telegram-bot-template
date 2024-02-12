3e32fef622b7ed3b382014f1a257d638df0f7d57
João Rafael <joaoraf@me.com>
2024-02-12 17:21:42 -0300

    feat: add explore command to bot main file

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,6 +1,7 @@
 import logging
 
 from bot.clients import bot
+from bot.flights import get_explore_flights
 
 logger = logging.getLogger(__name__)
 
@@ -8,6 +9,12 @@ logger = logging.getLogger(__name__)
 @bot.message_handler(commands=["start"])
 def start(message):
     bot.send_message(message.chat.id, "hi")
+
+
+@bot.message_handler(commands=["explore"])
+def explore(message):
+    flights = get_explore_flights()
+    bot.send_message(message.chat.id, flights)
 
 
 @bot.message_handler(func=lambda x: x)
